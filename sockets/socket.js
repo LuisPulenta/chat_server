a45c09039d9e230d1fd880c3e4714e87891de7f5
LuisPulenta <luisalbertonu@gmail.com>
2022-09-08 11:38:47 -0300

    Video 109

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,14 +1,20 @@
 const {io} =require('../index');
+const { comprobarJWT } = require('../helpers/jwt');
+const { usuarioConectado, usuarioDesconectado } = require('../controllers/socket');
 
 //Mensajes de Sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
-    client.on('disconnect',() => {
-        console.log('Cliente desconectado')
-    });
+    const [valido,uid]=comprobarJWT(client.handshake.headers['x-token'])
 
-    //  client.on('mensaje',(payload)=>{
-    //      console.log('Mensaje',payload);
-    //      io.emit('mensaje',{admin:'Nuevo mensaje'});
-    //  });
+    //----------- Verificar autenticado ------------
+    if(!valido){return client.disconnect();}
+
+    //----------- Cliente autenticado ------------
+    usuarioConectado( uid );
+
+    client.on('disconnect',() => {
+      console.log('Cliente desconectado');
+       usuarioDesconectado( uid );
+    });
   });
